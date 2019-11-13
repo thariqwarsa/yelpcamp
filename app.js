4c8aff2ade17bb2791667b1c439f28cd9d653221
thariqwarsa <thoriyama@gmail.com>
2019-11-13 00:07:34 +0000

    edit app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var commentsRoutes 	= require("./routes/comments"),
 
 //APP and DB CONFIG
 // mongoose.connect("mongodb://localhost:27017/yelpcamp_v12");
-mongoose.connet("mongodb+srv://thariqwarsa:" + process.env.DB_PASSWORD + "@cluster0-x1dzn.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://thariqwarsa:" + process.env.DB_PASSWORD + "@cluster0-x1dzn.mongodb.net/test?retryWrites=true&w=majority");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
